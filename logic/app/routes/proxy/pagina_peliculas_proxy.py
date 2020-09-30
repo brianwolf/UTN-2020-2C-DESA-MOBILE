8eb9adca6d3d45d00de743b8adf0bec6eadb5772
Brian Lobo <brian.lobo@moorea.io>
2020-09-30 11:40:01 -0300

    funciona el proxy

--- logic/app/routes/proxy/pagina_peliculas_proxy.py
+++ logic/app/routes/proxy/pagina_peliculas_proxy.py
@@ -1,6 +1,7 @@
-from flask import Blueprint, jsonify, render_template
-
+from flask import Blueprint, jsonify, render_template, request
 from logic.app.configs import config
+from logic.libs.logger.logger import log
+from pip._vendor.requests import get
 
 blue_print = Blueprint('pagina_peliculas', __name__,
                        url_prefix='/proxy/v1/themoviedb')
@@ -8,4 +9,11 @@ blue_print = Blueprint('pagina_peliculas', __name__,
 
 @blue_print.route('/<path:path>')
 def proxy(path):
-    return {'ruta': path}
+
+    host = config.PAG_PELICULAS_URL
+    sufijo_api_key = f'?api_key={config.PAG_PELICULAS_API_KEY}'
+
+    url_final = f'{host}{path}{sufijo_api_key}'
+    log().info(f'URL a redireccionar -> {url_final}')
+
+    return get(url_final).content
